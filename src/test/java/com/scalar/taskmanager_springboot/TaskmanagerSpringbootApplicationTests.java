500617bf0d3d3e17a3fb38c86eda5c2e4a5a9c6e
KC-002 <kc.praveenkumar002@gmail.com>
2024-08-11 20:22:11 +0530

    adding files

---
+++ src/test/java/com/scalar/taskmanager_springboot/TaskmanagerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalar.taskmanager_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
